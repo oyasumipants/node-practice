8063fb3126d3f71217ffd7b55c067ff12769b07e
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-27 23:33:36 +0900

    feat: add eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,28 @@ import globals from "globals";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    files: ["**/*.js"], 
+    languageOptions: 
+    {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    languageOptions: 
+    {
+      globals: {...globals.browser, ...globals.node}
+    }
+  },
+  {
+    "rules": {
+        "no-unused-vars": ["error", {
+            "vars": "all",
+            "args": "none"
+        }],
+        "no-console": [
+          "off"
+        ]
+    }
+}
 ];
